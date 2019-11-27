0df8b35293b819f556624c2b99d9b4325ec4b421
depaulo <adelsondepaulo@hotmail.com>
2019-11-27 15:22:45 -0300

    <Logica de programacao done>

--- LogicaDeProgramação.py
+++ LogicaDeProgramação.py
@@ -1,42 +1,13 @@
-class MultiplicationObject(object) :
-    def __init__(self, a, b, c) :
-        self._a = a 
-        self._b = b 
-        self._c = c 
-    
-    def multiplication(self) :
-        return self._a*self._b*self._c
-
-    @property
-    def a(self) :
-        return self._a
-
-    @property
-    def b(self) :
-        return self._b
-
-    @property
-    def c(self) :
-        return self._c
-
-    @a.setter
-    def a(self, a) :
-        self._a = a
-
-    @b.setter
-    def b(self, b) :
-        self._b = b
-
-    @c.setter
-    def c(self, c) :
-        self._c = c
+def Multiplos(multiplo1, multiplo2, limite) :
+    result = []
+    for each in range(limite) :
+        if each % multiplo1 == 0 and each % multiplo2 == 0 :
+            result.append(each)
+    return result
 
 
-if __name__ == '__main__' : #main program
-    obj = MultiplicationObject(1, 2, 3)
-    result = obj.multiplication()
+if __name__ == '__main__' :
+    result=Multiplos(3, 5, 1000)
     print(result)
-
-    obj = MultiplicationObject(5, 6, 7)
-    result = obj.multiplication()
+    result=Multiplos(6, 7, 1000)
     print(result)
